429c65a542d517ffc96041f3b47e374d5581ca14
Fernando Fernandez <ffmp777@yahoo.com>
2017-01-20 08:03:05 -0700

    managing css and js

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,11 @@ require('laravel-elixir-vue-2');
  |
  */
 
+//* elixir is our predefined mixins to perform common tasks.  Here we can use sass, less...it only needs to be added to mix.{sass, less...(only one tho)}
+//* Even tho this is avaailable to us right out of the box, dependencies still need to be added (package.json) with node and npm.
+//* Node packages (npm install) will be installed inside node_modules directory.
 elixir((mix) => {
     mix.sass('app.scss')
-       .webpack('app.js');
+       .webpack('app.js')
+       .version('css/app.css'); //* Compiles our scss and versions (gives a unique hash) the resulting file (cache busting).  You'll still need to add laravel's elixir function in the layout.blade.php file.
 });
